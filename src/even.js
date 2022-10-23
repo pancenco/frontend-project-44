ed193766f630851198d0d6a78c5367860342506d
Egor Panchenko <pancenco@yahoo.com>
2022-10-23 11:30:48 -0700

    added src for even

---
+++ src/even.js
@@ -0,0 +1,34 @@
+/* eslint-disable import/extensions, no-console */
+
+import readlineSync from 'readline-sync';
+
+const name = readlineSync.question(
+  'Welcome to the Brain Games!\nMay I have your name? ',
+);
+
+console.log(`Hello, ${name}!`);
+
+const getRandomInt = (max) => Math.floor(Math.random() * max);
+
+const getTypeOfNumber = () => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  let i = 0;
+  while (i < 3) {
+    const figure = getRandomInt(100);
+    console.log(`Question: ${figure}`);
+    const answer = readlineSync.question('Your answer: ');
+
+    if ((figure % 2 === 0 && answer === 'yes') || (figure % 2 !== 0 && answer === 'no')) {
+      console.log(`Your answer: ${answer}\nCorrect!`);
+      i += 1;
+    } if (figure % 2 === 0 && answer !== 'yes') {
+      console.log(`${answer} is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}!`);
+    } if (figure % 2 !== 0 && answer !== 'no') {
+      console.log(`${answer} is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+};
+
+export default getTypeOfNumber;
