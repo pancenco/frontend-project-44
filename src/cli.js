8334f5ffada9776d74a331b238cb848ead921377
Egor Panchenko <pancenco@yahoo.com>
2022-11-27 07:40:01 -0800

    files at begin

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/extensions, no-console */
+
+import readlineSync from 'readline-sync';
+
+const name = readlineSync.question(
+  'Welcome to the Brain Games!\nMay I have your name? ',
+);
+
+export default name;
